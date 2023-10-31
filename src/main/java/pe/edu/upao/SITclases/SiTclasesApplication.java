d73843f8edf9d9d816c24647dc9317f536d3a2dd
harrycas <harryjosecastrorodriguez@gmail.com>
2023-10-31 10:26:12 -0500

    first commit

---
+++ src/main/java/pe/edu/upao/SITclases/SiTclasesApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.SITclases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiTclasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiTclasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upao/SITclases/SiTclasesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.SITclases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiTclasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
